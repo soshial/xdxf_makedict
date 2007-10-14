7de511d1b587ea95ac8c0df91c3b5117ea38181e
Dushistov <dushistov@mail.ru>
2007-10-14 11:10:34 +0000

    remove autotools staff
    git-svn-id: https://xdxf.svn.sourceforge.net/svnroot/xdxf/trunk@73 9cfc4783-1b1d-0410-aaba-dcc21bd7f26c

--- mvsc/config.h
+++
@@ -1,6 +0,0 @@
-#define __attribute_warn_unused_result__ /**/
-#define DUMMY_FORMAT_NAME "dummy"
-#define CODECSDIR "."
-#define VERSION "0.3beta1"
-#define WEXITSTATUS(s) (s)
-#define __attribute__(e) /**/
